0b34115b72cf14317b12f084fa6416438978d90f
kzoacn <kzoacn@outlook.com>
2023-09-13 14:24:04 +0800

    tvs

--- tests/test_faest_tvs.hpp
+++ tests/test_faest_tvs.hpp
@@ -12,90 +12,48 @@
 namespace faest_tvs {
 
   extern const std::string message;
-
-  namespace faest_128s_tvs {
-    extern const std::array<uint8_t, 16 + 16> packed_sk;
-    extern const std::array<uint8_t, 16 + 16> packed_pk;
-    extern const std::array<uint8_t, 16> randomness;
-    extern const std::array<uint8_t, 5006> signature;
+ namespace faest_128s_tvs {
+extern const std::array<uint8_t, 32> packed_sk;
+extern const std::array<uint8_t, 86> packed_pk;
+extern const std::array<uint8_t, 16> randomness;
+extern const uint64_t signature_checksum;
   } // namespace faest_128s_tvs
 
   namespace faest_128f_tvs {
-    extern const std::array<uint8_t, 16 + 16> packed_sk;
-    extern const std::array<uint8_t, 16 + 16> packed_pk;
-    extern const std::array<uint8_t, 16> randomness;
-    extern const std::array<uint8_t, 6336> signature;
+extern const std::array<uint8_t, 32> packed_sk;
+extern const std::array<uint8_t, 86> packed_pk;
+extern const std::array<uint8_t, 16> randomness;
+extern const uint64_t signature_checksum;
   } // namespace faest_128f_tvs
 
   namespace faest_192s_tvs {
-    extern const std::array<uint8_t, 32 + 24> packed_sk;
-    extern const std::array<uint8_t, 32 + 32> packed_pk;
-    extern const std::array<uint8_t, 24> randomness;
-    extern const std::array<uint8_t, 12744> signature;
+extern const std::array<uint8_t, 48> packed_sk;
+extern const std::array<uint8_t, 142> packed_pk;
+extern const std::array<uint8_t, 24> randomness;
+extern const uint64_t signature_checksum;
   } // namespace faest_192s_tvs
 
   namespace faest_192f_tvs {
-    extern const std::array<uint8_t, 32 + 24> packed_sk;
-    extern const std::array<uint8_t, 32 + 32> packed_pk;
-    extern const std::array<uint8_t, 24> randomness;
-    extern const std::array<uint8_t, 16792> signature;
+extern const std::array<uint8_t, 48> packed_sk;
+extern const std::array<uint8_t, 142> packed_pk;
+extern const std::array<uint8_t, 24> randomness;
+extern const uint64_t signature_checksum;
   } // namespace faest_192f_tvs
 
   namespace faest_256s_tvs {
-    extern const std::array<uint8_t, 32 + 32> packed_sk;
-    extern const std::array<uint8_t, 32 + 32> packed_pk;
-    extern const std::array<uint8_t, 32> randomness;
-    extern const std::array<uint8_t, 22100> signature;
+extern const std::array<uint8_t, 64> packed_sk;
+extern const std::array<uint8_t, 190> packed_pk;
+extern const std::array<uint8_t, 32> randomness;
+extern const uint64_t signature_checksum;
   } // namespace faest_256s_tvs
 
   namespace faest_256f_tvs {
-    extern const std::array<uint8_t, 32 + 32> packed_sk;
-    extern const std::array<uint8_t, 32 + 32> packed_pk;
-    extern const std::array<uint8_t, 32> randomness;
-    extern const std::array<uint8_t, 28400> signature;
+extern const std::array<uint8_t, 64> packed_sk;
+extern const std::array<uint8_t, 190> packed_pk;
+extern const std::array<uint8_t, 32> randomness;
+extern const uint64_t signature_checksum;
   } // namespace faest_256f_tvs
 
-  namespace faest_em_128s_tvs {
-    extern const std::array<uint8_t, 16 + 16> packed_sk;
-    extern const std::array<uint8_t, 16 + 16> packed_pk;
-    extern const std::array<uint8_t, 16> randomness;
-    extern const std::array<uint8_t, 4566> signature;
-  } // namespace faest_em_128s_tvs
-
-  namespace faest_em_128f_tvs {
-    extern const std::array<uint8_t, 16 + 16> packed_sk;
-    extern const std::array<uint8_t, 16 + 16> packed_pk;
-    extern const std::array<uint8_t, 16> randomness;
-    extern const std::array<uint8_t, 5696> signature;
-  } // namespace faest_em_128f_tvs
-
-  namespace faest_em_192s_tvs {
-    extern const std::array<uint8_t, 24 + 24> packed_sk;
-    extern const std::array<uint8_t, 24 + 24> packed_pk;
-    extern const std::array<uint8_t, 24> randomness;
-    extern const std::array<uint8_t, 10824> signature;
-  } // namespace faest_em_192s_tvs
-
-  namespace faest_em_192f_tvs {
-    extern const std::array<uint8_t, 24 + 24> packed_sk;
-    extern const std::array<uint8_t, 24 + 24> packed_pk;
-    extern const std::array<uint8_t, 24> randomness;
-    extern const std::array<uint8_t, 13912> signature;
-  } // namespace faest_em_192f_tvs
-
-  namespace faest_em_256s_tvs {
-    extern const std::array<uint8_t, 32 + 32> packed_sk;
-    extern const std::array<uint8_t, 32 + 32> packed_pk;
-    extern const std::array<uint8_t, 32> randomness;
-    extern const std::array<uint8_t, 20956> signature;
-  } // namespace faest_em_256s_tvs
-
-  namespace faest_em_256f_tvs {
-    extern const std::array<uint8_t, 32 + 32> packed_sk;
-    extern const std::array<uint8_t, 32 + 32> packed_pk;
-    extern const std::array<uint8_t, 32> randomness;
-    extern const std::array<uint8_t, 26736> signature;
-  } // namespace faest_em_256f_tvs
 
 } // namespace faest_tvs
 
